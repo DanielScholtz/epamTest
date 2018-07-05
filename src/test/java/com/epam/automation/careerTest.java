b568b32d8fc4ef054990992bf8b93350a706b978
Daniel_Scholtz <scholtz.dani@gmail.com>
2018-07-05 16:27:00 +0200

    second commit

--- src/test/java/com/epam/automation/careerTest.java
+++ src/test/java/com/epam/automation/careerTest.java
@@ -1,12 +1,5 @@
 package com.epam.automation;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.FindBy;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class careerTest extends inside{
