9b0545c16e0dcf383069fc50f105c18223b5eee1
Daniel_Scholtz <scholtz.dani@gmail.com>
2018-07-06 13:02:49 +0200

    third commit

--- src/test/java/com/epam/automation/careerTest.java
+++ src/test/java/com/epam/automation/careerTest.java
@@ -8,9 +8,38 @@ public class careerTest extends inside{
         keywords("Test Automation");
         country("Hungary");
         city("Debrecen");
+        openSkillTab();
         skills("Software Test Engineering");
         searchButton();
         checkingResult("Currently we are looking for a Test Automation Engineer for our Debrecen office to make the team even stronger.");
+        Thread.sleep(1000);
+    }
+
+
+    @Test
+    public void searchForJob2() throws InterruptedException {
+        keywords("Developer");
+        country("Poland");
+        city("Katowice");
+        openSkillTab();
+        skills("Software Engineering");
+        skills("Software Architecture");
+        searchButton();
+        checkingResult("Currently we are looking for a .NET (Azure) Developer for our Katowice office to make the team even stronger.");
+        Thread.sleep(1000);
+    }
+
+    @Test
+    public void searchForJob3() throws InterruptedException {
+        keywords("Manager");
+        country("United States");
+        city("USA");
+        searchButton();
+        Thread.sleep(1000);
+        checkingResult("Manager-Intelligent Automation (RPA)-Business Consulting");
+        checkingResult("USA");
+        checkingResult("Hot");
+        checkingResult("You are strategic, resilient, engaging with people and a natural self-starter.");
         Thread.sleep(1000);
     }
 }
--- src/test/java/com/epam/automation/inside.java
+++ src/test/java/com/epam/automation/inside.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-public class inside{
+public class inside {
     public WebDriver driver;
 
     @BeforeMethod
@@ -18,32 +18,41 @@ public class inside{
         System.out.println(driver.getTitle());
     }
 
-    public void keywords (String message) {
+    public void keywords(String message) {
         WebElement keyword = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[1]/div[1]/section/div/div[4]/form/div/label/input"));
         keyword.sendKeys(message);
     }
 
     public void country(String country) {
         driver.findElement(By.xpath("//*[starts-with(@id,'select-box-location-')]")).click();
-        driver.findElement(By.cssSelector("[aria-label="+country+"]")).click();
+        driver.findElement(By.cssSelector("[aria-label=\"" + country + "\"]")).click();
     }
 
     public void city(String city) {
-        driver.findElement(By.xpath("//*[contains(@id, '"+city+"')]")).click();
+        if(city.toLowerCase() == "all") {
+            driver.findElement(By.xpath("//*[contains(@id, 'all_"  + city + "')]")).click();
+        } else {
+            driver.findElement(By.xpath("//*[contains(@id, '"  + city + "')]")).click();
+        }
     }
 
-    public void skills(String skill)  {
+    public void openSkillTab() {
         driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[1]/div[1]/section/div/div[4]/form/div/div[2]/div/div[1]/div[1]")).click();
-        driver.findElement(By.xpath("//li[contains(.,'"+skill+"')]")).click();
+    }
+
+    public void skills(String skill) {
+        WebElement check = driver.findElement(By.xpath("//*[@class='checkbox-custom-label' and contains(., '" + skill + "')]"));
+        check.click();
     }
 
     public void searchButton() {
         driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[1]/div[1]/section/div/div[4]/form/div/div[3]/button")).click();
     }
 
-    public void checkingResult(String description) {
-        assert driver.getPageSource().contains(description);
+    public void checkingResult(String text) {
+        assert driver.getPageSource().contains(text);
     }
+
 
     @AfterMethod
     public void closePage() {
