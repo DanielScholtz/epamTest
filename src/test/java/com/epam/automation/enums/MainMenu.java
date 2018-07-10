dffd4e0d6ca2546ef548e24199564f252b1fa4f0
Daniel_Scholtz <scholtz.dani@gmail.com>
2018-07-10 17:19:13 +0200

    fixes

---
+++ src/test/java/com/epam/automation/enums/MainMenu.java
@@ -0,0 +1,11 @@
+package com.epam.automation.enums;
+
+public enum MainMenu {
+    ;
+    public static final String WHAT_WE_DO_MENU = "/what-we-do";
+    public static final String HOW_WE_DO_IT_MENU = "/how-we-do-it";
+    public static final String OUR_WORK_MENU = "/our-work";
+    public static final String INSIGHTS_MENU = "/insights";
+    public static final String ABOUT_MENU = "/about";
+    public static final String CAREER_MENU = "/careers";
+}
--- src/test/java/com/epam/automation/inside.java
+++ src/test/java/com/epam/automation/inside.java
@@ -1,20 +1,56 @@
 package com.epam.automation;
 
+import com.epam.automation.enums.MainMenu;
 import org.openqa.selenium.By;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import static org.testng.Assert.assertTrue;
-
 public class inside {
-    public WebDriver driver;
+    /**
+     * There are some Page Objects and contsuctors
+     * @param driver to initalize our browser
+     * @param country is here because we use it in more methods
+     * @param city is here because we use it in more methods
+     * @param logo for checking the site loaded correctly
+     * @param locationArrow opening location tab
+     * @param defaultLocation to select All Location by default
+     * @param keywordsInput to write inputs in the keywords place
+     * @param skillsTabArrow to open the skills tab
+     * @param searchButton to click the search button
+     * @param searchResult for checking the search result
+     * @param sortByDate is to reorder the search results by date
+     */
+
+    private WebDriver driver;
+    private String country = "";
+    private String city = "";
+    @FindBy(xpath = "//*[contains(@src, 'logo_white-blue.svg')]")
+    private WebElement logo;
+    @FindBy(xpath = "//*[starts-with(@id,'select-box-location-')]")
+    private WebElement locationArrow;
+    @FindBy (xpath = "//*[contains(@id, 'all_locations')]")
+    private WebElement defaultLocation;
+    @FindBy (xpath = "//*[starts-with(@class, 'job-search__input')]")
+    private WebElement keywordInput;
+    @FindBy (xpath =  "/html/body/div[1]/div[3]/div[1]/div[1]/section/div/div[4]/form/div/div[2]/div/div[1]/div[1]")
+    private WebElement skillsTabArrow;
+    @FindBy (xpath = "/html/body/div[1]/div[3]/div[1]/div[1]/section/div/div[4]/form/div/div[3]/button")
+    private WebElement searchButton;
+    @FindBy (className = "search-result__item-name")
+    private WebElement searchResult;
+    @FindBy (css = "li.search-result__sorting-item:nth-child(2)")
+    private WebElement sortByDate;
 
     @BeforeMethod
     public void openBrowser() {
@@ -22,68 +58,70 @@ public class inside {
         driver = new FirefoxDriver();
         driver.get("https://www.epam.com");
         System.out.println(driver.getTitle());
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        PageFactory.initElements(driver, this);
     }
 
     public void navigation(String Menu) {
-        // menüpontokra tud lépni enumból kiszedett címekkel, almenüket jelenleg nem raktam bele
-        // az oldal sikeres betöltését azzal ellenőrzi, hogy megjelent e a bal felső sarokban lévő epam logó
-        driver.findElement(By.xpath("//a[@href='" + Menu + "']")).click();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        System.out.println(driver.getTitle());
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        assertTrue(driver.findElement(By.xpath("//*[contains(@src, 'logo_white-blue.svg')]")).isDisplayed());
-        /* Updatelve lett az Epam oldal, ami most már az országot és a várost automatikusan kitölti az általad szedett adatokból
-        ezért előre beállítom, hogy mindig állítsa vissz alapértékre a locationt az egyszerűség kedvéért
+        /**
+         * Navigation between main menus
+         * @param Menu getting a String from enums which refers to a main menu
+         * Checking the site load correctly with the logo display
+         * with the latest Epam website update we always set the location back to All location by default
+         * since its get the location from our ip address
          */
-        if (Menu.equals(com.epam.automation.enums.Menu.CAREER)) {
-            driver.findElement(By.xpath("//*[starts-with(@id,'select-box-location-')]")).click();
-            driver.findElement(By.xpath("//*[contains(@id, 'all_locations')]")).click();
+        driver.findElement(By.xpath("//a[@href='" + Menu + "']")).click();
+        System.out.println(driver.getTitle());
+        assert(logo.isDisplayed());
+        if (Menu.equals(MainMenu.CAREER_MENU)) {
+            locationArrow.click();
+            defaultLocation.click();
         }
     }
 
     public void keywords(String message) {
-        WebElement keyword = driver.findElement(By.xpath("//*[starts-with(@class, 'job-search__input')]"));
-        keyword.sendKeys(message);
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        keywordInput.sendKeys(message);
     }
 
     public void location(String country, String city) {
-        // ország, város kiválasztása, ha a városhoz ugyanazt írjuk, mint az ország, akkor az országban lévő összes munkát kilistázza
-        // (ez alól kivétel Amerika, ahol USA-t kell írni az ország ismétlése helyett)
-        driver.findElement(By.xpath("//*[starts-with(@id,'select-box-location-')]")).click();
+        /**
+         * This method is stands for choosing the location
+         * if we want to list all cities of the country we have to use the same name as the country
+         * there is only one exception
+         * when the country is United States, we should use USA at city variable
+         */
+        locationArrow.click();
         driver.findElement(By.cssSelector("[aria-label=\"" + country + "\"]")).click();
         if (city == country) {
             driver.findElement(By.xpath("//*[contains(@id, 'all_" + city + "')]")).click();
         } else {
             driver.findElement(By.xpath("//*[contains(@id, '" + city + "')]")).click();
         }
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
 
     public void openSkillTab() {
-        /*külön vettem egyelőre a lista megnyitást és a skillek kiválasztását, mert több skill kiválasztása esetén errort ad, mivel 2xer akarja megnyitni.
-        if-el vizsgáltam htmlként, hogy van e függőleges lista megnyitva, de a keywords input rész megzavarja az ajánlott keywords kiegészítésekkel, így
-        false infót ad az állapotról
-        */
-        driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[1]/div[1]/section/div/div[4]/form/div/div[2]/div/div[1]/div[1]")).click();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        /**
+         * Its for opening the skills tab
+         * its useful when choosing more skills and want to avoid errors
+         */
+        skillsTabArrow.click();
     }
 
     public void skills(String skill) {
         driver.findElement(By.xpath("//*[@class='checkbox-custom-label' and contains(., '" + skill + "')]")).click();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
-    public void searchButton() {
-        driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[1]/div[1]/section/div/div[4]/form/div/div[3]/button")).click();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    public void clickOnSearchButton() {
+        searchButton.click();
+        WebDriverWait wait =  new WebDriverWait(driver,20);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("search-result__item-name")));
     }
 
     public void showJobsWithLocation() {
-        //kilistázza a munkákat munkavégzés helyével együtt
-        String jobs = "";
+        /**
+         * Making a log of jobs with location from search result
+         */
+        String jobs;
         String loc = "";
         List<WebElement> job = driver.findElements(By.className("search-result__item-name"));
         for (WebElement ele : job) {
@@ -95,37 +133,37 @@ public class inside {
             }
             System.out.println(jobs + " " + loc);
         }
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
-    public void sortJobsByDate() throws  Exception {
-    try {
-        driver.findElement(By.cssSelector("li.search-result__sorting-item:nth-child(2)")).click();
-        /*egyelőre Thread.sleepet használok, mert az implicitlyWaitnél vagy a sleep nélküli állapotnál folyamatosan
-        StaleElementReferenceException errort kapok. Az oldal az update óta lassan frissíti a munkákat dátum alapján, ezért
-        a listázás közben történt a frissítés, ami errort okoz a tesztben. A try/catch blokkot benne hagytam biztonság kedvéért.
+    public void sortJobsByDate() throws StaleElementReferenceException {
+        /**
+         * Sorting the jobs by date
+         * the try/catch block is for not refreshing the page in time
+         * sadly the test cannot be verified from id
+         * because if they update a job it become the newest but with the old id
          */
-        Thread.sleep(1000);
-        List<WebElement> allElements = driver.findElements(
-                By.className("search-result__item-name")
-        );
-        for (WebElement element : allElements) {
-            String idList = element.getAttribute("href");
-            /*feltételeztem, hogy id szerint csökkenő sorrend lenne, ezzel ellenőrízhető a dátum szerinti sorrend, de vannak régebbi jobok
-              amik frissítve lettek, így habár id szerint kisebbek, de mégis fentebb kerültek, ezért nem is bontottam tovább substringgal, hogy
-              id-kat hasonlítson össze */
-            System.out.println(idList);
+        try {
+            sortByDate.click();
+            WebDriverWait wait =  new WebDriverWait(driver,20);
+            wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("li.search-result__sorting-item:nth-child(1)")));
+            List<WebElement> allElements = driver.findElements(
+                    By.className("search-result__item-name")
+            );
+            for (WebElement element : allElements) {
+                String idList = element.getAttribute("href");
+                System.out.println(idList);
+            }
+        } catch (StaleElementReferenceException e) {
+            System.out.println("error in sortJobsByDate" +e);
         }
-    } catch (Exception e) {
-        System.out.println("error in sortJobsByDate" +e);
-    }
     }
 
 
     public void checkingResult(String text) {
-        //megkeresi az oldalon található szövegben, hogy pontos-e a találat
+        /**
+         * Checking if the search result is correct by the description of the job
+         */
         assert driver.getPageSource().contains(text);
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
 
