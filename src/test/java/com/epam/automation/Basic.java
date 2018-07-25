5d64f4d803ae432531221a0015f9fc39e463d11d
Daniel_Scholtz <scholtz.dani@gmail.com>
2018-07-25 11:22:36 +0200

    OOP

--- src/test/java/com/epam/automation/Basic.java
+++ src/test/java/com/epam/automation/Basic.java
@@ -1,19 +1,19 @@
-package com.epam.automation;
-
-import com.epam.automation.core.DriverManager;
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.*;
-
-public class Basic {
-    WebDriver driver;
-
-    @BeforeSuite
-    public void setupBrowser() {
-        driver = DriverManager.getDriver(driver);
-    }
-
-    @AfterSuite
-    public void tearDown() {
-        driver.quit();
-    }
-}
+//package com.epam.automation;
+//
+//import com.epam.automation.core.DriverManager;
+//import org.openqa.selenium.WebDriver;
+//import org.testng.annotations.*;
+//
+//public class Basic {
+//    WebDriver driver;
+//
+//    @BeforeSuite
+//    public void setupBrowser() {
+//        driver = DriverManager.getDriver(driver);
+//    }
+//
+//    @AfterSuite
+//    public void tearDown() {
+//        driver.quit();
+//    }
+//}
